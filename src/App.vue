4779bf5fe6fe48290fd7bad023d42109d48eefb7
Adam <adamskoullos@gmail.com>
2021-03-22 06:08:37 +0000

    created responsive breakpoints for side nav

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,47 @@
 <template>
   <div class="container-fluid">
     <div class="row main-row">
-      <div class="col-2 side-nav">
-        <nav class="navbar navbar-expand-md ps-3 pe-3 navbar-light bg-light">
-          <div class="nav-container-div">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse main-nav" id="navbarSupportedContent">
-              <div class="theme ">
+      <div class="col-2 side-nav" v-if="showSideNav">
+            <nav>
+              <div class="logo">
+                <router-link :to="{ name: 'MyProjects' }" class=""><h3>ProDev</h3></router-link>
+              </div>
+              <div class="theme">
                 <div class="form-check form-switch">
                   <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
                 </div>
               </div>
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
-                <li class="nav-item">
-                  <span class="btn" @click="toggleSideChat">Team Chat</span>
+              <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item" @click="toggleSideChat">
+                  <span >Team Chat</span>
                 </li>
                 <li class="nav-item">
-                  <router-link :to="{ name: 'TeamProjects' }" class=" btn" aria-current="page">Team Projects</router-link>
+                  <router-link :to="{ name: 'TeamProjects' }" class="" aria-current="page">Team Projects</router-link>
                 </li>
                 <li class="nav-item">
-                  <router-link :to="{ name: 'MyProjects' }" class=" btn" aria-current="page">My Projects</router-link>
+                  <router-link :to="{ name: 'MyProjects' }" class="" aria-current="page">My Projects</router-link>
                 </li>
                 <li class="nav-item">
-                  <router-link :to="{ name: 'NewProject' }" class=" btn" aria-current="page">New Project</router-link>
+                  <router-link :to="{ name: 'NewProject' }" class="" aria-current="page">New Project</router-link>
                 </li>
                 <li class="nav-item">
-                  <router-link :to="{ name: 'Bugs' }" class=" btn" aria-current="page">Bugs</router-link>
+                  <router-link :to="{ name: 'Bugs' }" class="" aria-current="page">Bugs</router-link>
                 </li>
                 <li class="nav-item">
-                  <router-link :to="{ name: 'NewBug' }" class=" btn" aria-current="page">New Bug</router-link>
+                  <router-link :to="{ name: 'NewBug' }" class="" aria-current="page">New Bug</router-link>
+                </li>
+                <li class="nav-item">
+                  <a class="logout">Logout</a>
                 </li>
               </ul>
-              <button class="btn logout">Logout</button>
-            </div>
-          </div>
-        </nav>
+            </nav>
+            
       </div>
       <div class="col main-col">
         <div class="row top-bar-row">
           <div class="col top-bar-col">
-            <h1>Top bar</h1>
             <div class="user">
-              <span>User Name</span>
+              <h2>Top bar</h2>
             </div>
           </div>
         </div>
@@ -62,18 +60,46 @@
 
 <script>
 import { ref } from '@vue/reactivity'
+import { onBeforeMount, onUnmounted } from '@vue/runtime-core'
 
 export default {
 
     setup(){
         const showSideChat = ref(true)
         const user = ref(true)
+        const showSideNav = ref(false)
+
+        onBeforeMount(()=> {
+          window.addEventListener('load', ()=> {
+            if(visualViewport.width < 900){
+              showSideNav.value = false
+            }
+            if(visualViewport.width > 900){
+              showSideNav.value = true 
+            }
+          }),
+
+          window.addEventListener('resize', () =>{
+            if(visualViewport.width < 900){
+              showSideNav.value = false
+            }
+            if(visualViewport.width > 900){
+              showSideNav.value = true 
+            }
+          })
+        })
+
+        onUnmounted(() => {
+          window.removeEventListener()
+        })
 
         const toggleSideChat = () => {
             showSideChat.value = !showSideChat.value
         }
 
-        return { toggleSideChat, showSideChat, user }
+        
+
+        return { toggleSideChat, showSideChat, user, showSideNav }
     }
 }
 
@@ -89,26 +115,24 @@ export default {
 }
 
 /* Basic layout and responsiveness for main window*/ 
-nav{
-  margin: 0px;
-  border-radius: 10px;
-  box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
-  border: 1px solid  var(--secondary);
-}
+
 .side-nav{
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  background: chartreuse;
+  align-items: stretch;
+  background: #fff;
+  max-width: 200px;
+  margin: 0px;
+  border-radius: 0px;
+  box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
+  border: 1px solid  var(--secondary);
 }
 .main-row{
   max-height: 100vh;
-  background: crimson;
 }
 .main-col{
   min-height: 100vh;
-  background: gold;
   display: flex;
   flex-direction: column;
 }
@@ -138,6 +162,7 @@ nav{
 /* Inner layout styling for side nav elements */
 .navbar-expand-md .navbar-nav{ 
     flex-direction: column;
+    
 }
 .navbar{
   display: flex;
@@ -152,10 +177,59 @@ nav{
   justify-content: space-between;
 }
 .main-nav{
+  flex:1;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-around;
+  min-height: 100vh;
+}
+.logo{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 12px; 
+}
+.nav-item{
+  background: var(--secondary);
+  color: var(--primary);
+  border-radius: 8px;
+  border: 0;
+  padding: 10px 12px;
+  font-weight: 600;
+  cursor: pointer;
+  display: inline-block;
+  box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
+  border: 1px solid  var(--secondary);
+  width: 100%;
+}
+a{
+  color: var(--primary);
+}
+.theme{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.theme div{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 15px auto;
+}
+.theme input{
+  margin-right: -2px;
+}
+.navbar-nav{
+  flex: 1;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: space-between;
-  min-height: 100vh;
+  justify-content: start;
+}
+ul li {
+  margin: 10px auto 10px 0;
+  min-width: 130px;
 }
 </style>
